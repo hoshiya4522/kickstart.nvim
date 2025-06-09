84947ae23cf01bec08b6abe432b0d3db309eb032
hoshiya4522 <dhrubodev@gmail.com>
2025-06-09 12:32:57 +0600

    Last commit from rangpur

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -1,7 +1,9 @@
 vim.o.nu = true
 vim.o.rnu = true
-vim.o.ts = 4
 vim.opt.termguicolors = true
-vim.g.rust_recommended_style = 0 -- this one is a gem
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.o.expandtab = true -- retab
 
 return {}
